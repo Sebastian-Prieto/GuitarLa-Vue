7d081f9ddf3b5f9cfad160d5238ade0813a0a6da
Sebastian <sbprietoc@gmail.com>
2023-11-27 21:45:04 -0500

    Add props and state

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,20 @@
+<script setup>
+        
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div class="col-4">
+                    <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">{{  }}</h3>
+                    <p>{{  }}</p>
+                    <p class="fw-black text-primary fs-3">$299</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>
+            </div><!-- FIN GUITARRA -->
+</template>
