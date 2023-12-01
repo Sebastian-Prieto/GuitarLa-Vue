3cad649d941a00b9ec3481353c4cd4e52318cc0a
Sebastian <sbprietoc@gmail.com>
2023-11-30 22:26:32 -0500

    Add functions

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,11 @@ import Footer from './components/Footer.vue'
 
 const guitarras = ref([]) // State de las guitarras
 const carrito = ref([]) // State carrito de compras
+const guitarra = ref({}) // State de la guitarra principal
 
 onMounted(() => {
     guitarras.value = db;
+    guitarra.value = db[3]
 })
 
 const agregarCarrito = (guitarra) => {
@@ -21,13 +23,39 @@ const agregarCarrito = (guitarra) => {
         carrito.value.push(guitarra) // Push añade un elemento a un arreglo al final - Modifica el arreglo original ☢️
     }
 }
+
+const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+}
+
+const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+}
+
+const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+}
+
+const vaciarCarrito = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.cantidad != cantidad)
+}
 </script>
     
 <template>
-    <Header :carrito="carrito" />
+    <Header
+    :carrito="carrito"
+    :guitarra="guitarra"
+    @incrementar-cantidad="incrementarCantidad"
+    @decrementar-cantidad="decrementarCantidad" 
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    />
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
-
         <div class="row mt-5">
             <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" @agregar-carrito="agregarCarrito" />
         </div>
